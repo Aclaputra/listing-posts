a4684a9ddfa368f8fae0561dd1a1f16eb0af3cbd
Aclaputra <muhamadacla22@gmail.com>
2023-04-11 23:26:59 +0700

    adding bootstrap vite

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,11 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Laragigs</title>
+
+  @vite(['resources/js/app.js'])
 </head>
 <body>
-  <h1>laragigs</h1>
+  <h1>LinkedOut</h1>
   @yield('content')
-
-
 </body>
 </html>
